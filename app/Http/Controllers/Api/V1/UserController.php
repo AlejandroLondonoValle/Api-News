948a8e910f66bed94f12c347197d629b8faf8f8e
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-11-12 08:08:10 -0500

    feat: se crean todos los controladores y se configuran las rutas con un prefijo y un middleware para protegerla

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -5,52 +5,86 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 use function PHPUnit\Framework\isEmpty;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $users = User::all();
-        if ($users.isEmpty()) {
+        if ($users->isEmpty()) {
             return response()->json(['message' => 'No users found'], 204);
         }
         return response()->json($users);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'is_admin' => ['required', 'boolean'],
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'last_name' => $validatedData['last_name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'is_admin' => false,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+
+        // Devolver la respuesta
+        return response()->json($user, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
-        //
+        $user = User::find($id);
+        if ($user === null) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        return response()->json($user);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'last_name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|string|email|max:255|unique:users,email,' . $id,
+            'password' => 'sometimes|string|min:8',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->update([
+            'name' => $validatedData['name'] ?? $user->name,
+            'last_name' => $validatedData['last_name'] ?? $user->last_name,
+            'email' => $validatedData['email'] ?? $user->email,
+            'password' => isset($validatedData['password']) ? Hash::make($validatedData['password']) : $user->password,
+        ]);
+
+        // Devolver la respuesta con el usuario actualizado
+        return response()->json($user);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $user = User::find($id);
+        if ($user === null) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        $user->delete();
+        return response()->json(['message' => 'User deleted'], 204);
     }
 }
